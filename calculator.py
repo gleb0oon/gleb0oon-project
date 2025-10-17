d17c7943745d531c0c03fbe98779ff8cf6aea503
gleb0oon <gleb1307@icloud.com>
2025-10-17 13:08:57 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,5 +3,5 @@ print("Калькулятор")
 num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
 print(f"Вы ввели: {num1} и {num2}")
-result = num1 + num2
-print(f"Результат сложения: {num1} + {num2} = {result}")
+result = num1 - num2
+print(f"Результат вычитания: {num1} + {num2} = {result}")
